b20adb065afb0396ba92fead92957a5c99a26d9b
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-12-11 13:47:36 +0700

    added animation and some text content DOM manipulation

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let activityBtn = document.getElementById('activity-btn');
+let title = document.getElementById('title');
 let apiUrl = 'https://apis.scrimba.com/bored/api/activity';
 let displayActivity = document.getElementById('activity-name');
+let emoji = document.querySelector('.emoji');
 
 activityBtn.addEventListener('click', () => {
   fetch(apiUrl)
@@ -8,12 +10,16 @@ activityBtn.addEventListener('click', () => {
     .then((data) => {
       displayActivity.textContent = data.activity;
 
+      title.textContent = 'BrightIdea Bot';
+
       displayActivity.classList.add('activity-colored');
 
       if (data.price > 0) {
         displayActivity.textContent += ' ($)';
+        emoji.classList.add('animate');
       } else {
-        return null;
+        emoji.classList.add('animate');
       }
     });
+  emoji.classList.remove('animate');
 });
